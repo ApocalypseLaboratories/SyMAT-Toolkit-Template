94c62656b459e6813ef58df1232b6a647c9d59a8
skylarmt <apocalypselabs@gmail.com>
2015-04-08 16:41:06 -0600

    First commit

---
+++ src/com/symatapp/toolkit/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.symatapp.toolkit;
+
+/**
+ * Sample class. Public functions available from scripts, etc. using the
+ * variable mapped in meta.txt.
+ */
+public class HelloWorld {
+    
+    public String world() {
+        return "Hello World!";
+    }
+    
+}
